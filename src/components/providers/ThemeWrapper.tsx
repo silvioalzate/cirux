7dc000bb1c61ff8a568f1a7d3e4dbba19d595218
silvioalzate <silvioalzate@gmail.com>
2026-06-27 14:41:40 -0500

    unzipgit

---
+++ src/components/providers/ThemeWrapper.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+
+export function ThemeWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem={false}
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
