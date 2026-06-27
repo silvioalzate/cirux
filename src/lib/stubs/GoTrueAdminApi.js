154deef5452d7f17953e3ee77b05097642e77b8d
silvioalzate <silvioalzate@gmail.com>
2026-06-27 14:42:30 -0500

    unzipgit

---
+++ src/lib/stubs/GoTrueAdminApi.js
@@ -0,0 +1,3 @@
+// Stub module to prevent GoTrueAdminApi from being bundled in client builds
+// This is an admin-only API that should never ship to the browser
+export default class GoTrueAdminApi {}
